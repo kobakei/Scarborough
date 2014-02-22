7aefd32e200883c939bb524d20570b4708339df2
takovine <takovine@gmail.com>
2014-02-22 21:06:34 +0900

    layout fixed

--- popup/popup.js
+++ popup/popup.js
@@ -16,8 +16,20 @@
 	}
 	
 	function setcardimg() {
-		$('.card').each(function() {
-			$(this).css('background-image','url(http://31.media.tumblr.com/78ce1831f575f06a6ca966ee2c9198f1/tumblr_n10nb0TY4u1st5lhmo1_1280.jpg)');
+		
+		var imgList = [
+			"http://25.media.tumblr.com/8fb80a2a6111ffbf05defc3a758a2f8a/tumblr_n10n4rpjvZ1st5lhmo1_1280.jpg",
+			"http://31.media.tumblr.com/78ce1831f575f06a6ca966ee2c9198f1/tumblr_n10nb0TY4u1st5lhmo1_1280.jpg",
+			"http://25.media.tumblr.com/d4955c5fb31743bd0740f5001adafb79/tumblr_n10n3wfcrl1st5lhmo1_1280.jpg",
+			"http://25.media.tumblr.com/18e25cdcdaaced3b2b8a467724720ece/tumblr_n10n1wmxiS1st5lhmo1_1280.jpg"
+		];
+		
+		var img = "http://25.media.tumblr.com/3f62ba4c56724f227dba2b4415a61049/tumblr_myebqysYZ81st5lhmo1_1280.jpg";
+		
+		$('.card').each(function(i) {
+			var num = Math.floor(Math.random()*4);
+			$(this).css('background-image','url(' + imgList[num] +')');
+			//$(this).css('background-image','url(' + img +')');
 		});	
 	}
 	
