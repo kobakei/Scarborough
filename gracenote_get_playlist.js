0cdab96680b028741b8f21445724493b0a7cca3a
Keisuke Kobayashi <k.kobayashi.122@gmail.com>
2014-02-22 17:29:10 +0900

    Added artist search

--- gracenote_get_playlist.js
+++ gracenote_get_playlist.js
@@ -1,20 +1,75 @@
-console.log("Hello bg.js");
+console.log("*** Getting playlist ***");
 
-var CLIENT_ID = "3868672-53AC3535785097C37E0B299756B825FB";
-var REGISTER_ENDPOINT = "https://c3868672.web.cddbp.net/webapi/json/1.0/register?client=" + CLIENT_ID;
+var attribute = "RADIOGENRE";
 
 // 保存したUser IDを取得
+var userId = localStorage["gracenote_user_id"];
 
-// Gracenoteを叩いて、
-var xhr = new XMLHttpRequest();
-xhr.open("GET", REGISTER_ENDPOINT, true);
-xhr.onreadystatechange = function() {
-  if (xhr.readyState == 4) {
-    // JSON.parse does not evaluate the attacker's scripts.
-    var resp = JSON.parse(xhr.responseText);
-    console.log(resp);
+var CLIENT_ID = "3868672-53AC3535785097C37E0B299756B825FB";
+var FIELD_ENDPOINT = "https://c3868672.web.cddbp.net/webapi/json/1.0/radio/fieldvalues?fieldname=" + attribute + "&client=" + CLIENT_ID + "&user=" + userId;
+
+var RETURN_COUNT = 5;
+var RYTHM_ENDPOINT = "https://c3868672.web.cddbp.net/webapi/json/1.0/radio/create?client=" + CLIENT_ID + "&user=" + userId + "&return_count=" + RETURN_COUNT;
+
+// Gracenoteを叩いて、Rythm APIフィールドリストを取得
+function getFieldList() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", FIELD_ENDPOINT, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      // JSON.parse does not evaluate the attacker's scripts.
+      var text = xhr.responseText;
+      console.log(text);
+      var resp = JSON.parse(text);
+      console.log(resp);
+    }
   }
+  xhr.send();
 }
-xhr.send();
 
-console.log("Completed!");
+// ジャンル・ムード・時代でトラックリストを取得します
+function getTrackListByRythm(genre, mood, era) {
+  var url = RYTHM_ENDPOINT;
+  if (genre) {
+    url = url + "&genre=" + genre;
+  }
+  if (mood) {
+    url = url + "&mood=" + mood;
+  }
+  if (era) {
+    url = url + "&era=" + era;
+  }
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      // JSON.parse does not evaluate the attacker's scripts.
+      var text = xhr.responseText;
+      var resp = JSON.parse(text);
+      console.log(resp);
+    }
+  }
+  xhr.send();
+}
+
+// アーティスト名・アルバム名で取得
+function getTrackListByWeb(artist, album) {
+  var url = RYTHM_ENDPOINT;
+  if (artist) {
+    url = url + "&artist_name=" + artist;
+  }
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      // JSON.parse does not evaluate the attacker's scripts.
+      var text = xhr.responseText;
+      var resp = JSON.parse(text);
+      console.log(resp);
+    }
+  }
+  xhr.send();
+}
+
+getTrackListByRythm(36060, null, null);
+//getTrackListByWeb("arctic monkeys");
