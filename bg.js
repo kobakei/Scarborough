a661674d4f2974ce6f83230fb6bf893dec7e9ee7
Keisuke Kobayashi <k.kobayashi.122@gmail.com>
2014-02-22 16:14:45 +0900

    Impl get gracenote ID

--- bg.js
+++ bg.js
@@ -1,1 +1,25 @@
 console.log("Hello bg.js");
+
+var CLIENT_ID = "3868672-53AC3535785097C37E0B299756B825FB";
+var REGISTER_ENDPOINT = "https://c3868672.web.cddbp.net/webapi/json/1.0/register?client=" + CLIENT_ID;
+
+// Gracenoteを叩く
+/*
+$.get(REGISTER_ENDPOINT, function(data){
+  console.log("*** response ***");
+  console.log(data);
+});
+*/
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", REGISTER_ENDPOINT, true);
+xhr.onreadystatechange = function() {
+  if (xhr.readyState == 4) {
+    // JSON.parse does not evaluate the attacker's scripts.
+    var resp = JSON.parse(xhr.responseText);
+    console.log(resp);
+  }
+}
+xhr.send();
+
+console.log("Completed!");
