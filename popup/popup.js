24ba9c3e8ae81a19fa45b6426371f43bc2eec914
takovine <takovine@gmail.com>
2014-02-22 20:21:21 +0900

    layout fixed

--- popup/popup.js
+++ popup/popup.js
@@ -1,38 +1,32 @@
 (function($) {
 	$(document).ready(function() {
-		autoPlay();
+		eventAdd();
 	});
 
-	function autoPlay() {
-		$('#spotify').load(function() {
-			setTimeout(function(){
-				play();
-			}, 1000);
-
-			function play() {
-				var e = document.createEvent('MouseEvents');
-				e.initMouseEvent('click', // type
-				true, // bubbles
-				true, // cancelable
-				window, // view
-				0, // detail（クリック数）
-				50, // screenX（デバイス全体における座標）
-				50, // screenY
-				0, // clientX（ブラウザ表示域における座標）
-				0, // clientY
-				false, // ctrlKey
-				false, // altKey
-				false, // shiftKey
-				false, // metaKey
-				0, // button（0 が左、1 が中、2 が右クリック）
-				null // relatedTarget
-				);
-				var n = document.getElementById("spotify");
-				// クリックのターゲットにしたいノード
-				n.dispatchEvent(e);
-			}
-
+	function eventAdd() {
+		$(document).on('click', '.card', function(e) {
+			makelist();
 		});
+
+		$(document).on('click', '#back', function(e) {
+			back();
+		});
+
+	}
+
+	function makelist() {
+
+		$('#list').slideUp();
+		$('#playlist').show();
+
+		//このURLをAPIでかえす
+		var url = 'https://embed.spotify.com/?uri=spotify:user:erebore:playlist:788MOXyTfcUb1tdw4oC7KJ';
+		$('<iframe />').attr('src', url).attr('frameborder', '0').attr('allowtransparency', 'true').attr('width', '300').attr('height', '380').appendTo('#playlist');
 	};
+
+	function back() {
+		$('#playlist').hide().find('iframe').remove();
+		$('#list').show();
+	}
 
 })(jQuery);
