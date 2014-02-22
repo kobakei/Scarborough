01c8cfa0d9d4f75b2b405dd7bcd5f331e2b9fc00
Keisuke Kobayashi <k.kobayashi.122@gmail.com>
2014-02-22 16:31:05 +0900

    Fixed get user id

---
+++ gracenote_get_id.js
@@ -0,0 +1,24 @@
+console.log("*** Getting Gracenote User ID ***");
+
+var CLIENT_ID = "3868672-53AC3535785097C37E0B299756B825FB";
+var REGISTER_ENDPOINT = "https://c3868672.web.cddbp.net/webapi/json/1.0/register?client=" + CLIENT_ID;
+
+// UserIDをストレージに保存する
+function saveGracenoteUserId(user_id) {
+  localStorage["gracenote_user_id"] = user_id;
+}
+
+// Gracenoteを叩く
+var xhr = new XMLHttpRequest();
+xhr.open("GET", REGISTER_ENDPOINT, true);
+xhr.onreadystatechange = function() {
+  if (xhr.readyState == 4) {
+    // JSON.parse does not evaluate the attacker's scripts.
+    var resp = JSON.parse(xhr.responseText);
+    console.log(resp);
+    var userId = resp["RESPONSE"][0]["USER"][0]["VALUE"];
+    console.log("User ID = " + userId);
+    saveGracenoteUserId(userId);
+  }
+}
+xhr.send();
